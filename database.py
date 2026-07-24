51419439127762d1f2798459c82bdbb3ca7cad98
Roent_CAT <ryukrjoshi@gmail.com>
2026-07-24 21:54:31 +0530

    Remove database connection URL from database.py
    Removed the database connection URL for security reasons.

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_BVhpJKHP0G8m@ep-bold-meadow-az46s63a-pooler.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+SQLALCHEMY_DATABASE_URL = ""
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
